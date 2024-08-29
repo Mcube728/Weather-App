4ae49ce03e2f281233d3c51d462c5f1f94f2c2f2
Mcube728 <boredAstronaut728@protonmail.com>
2024-08-29 17:31:55 +0530

    Updated weather module for forecast functionality

--- weather.py
+++ weather.py
@@ -6,15 +6,32 @@ import os
 load_dotenv()
 
 def getCurrentWeather(city='Mumbai'):
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
-    weatherData = requests.get(request_url).json()
+    api_key = os.getenv('API_KEY')
+    api_endpoint = 'https://api.openweathermap.org/data/2.5/weather'
+    forecast_params = {
+        'appid': api_key,
+        'q': city,
+        'units': 'metric'
+    }
+    weatherData = requests.get(api_endpoint, params=forecast_params).json()
     return weatherData
+
+def getCityForecast(city='Mumbai'):
+    api_key = os.getenv('API_KEY')
+    forecast_params = {
+        'appid': api_key,
+        'q': city,
+        'units': 'metric'
+    }
+    api_forecast_endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
+    forecast_data = requests.get(api_forecast_endpoint, params=forecast_params).json()
+    return forecast_data
 
 if __name__ == '__main__':
     print('\n*** Get Current Weather Conditions *** ')
     city = input('Please Enter City Name: ')
     if not bool(city.strip()): 
         city = 'Mumbai'     # Check for empty strings
-    weather = getCurrentWeather(city)
+    weather = getCityForecast(city)
     print('\n')
     pprint(weather)
